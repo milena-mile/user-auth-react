c774268832fecf2828d8d65981ab186dbec231ce
Elena <elena.bakumenko92@gmail.com>
2024-05-24 19:09:36 +0300

    Changed images pathes

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -4,7 +4,7 @@ const Error = () => {
     return (
         <div className="b-error">
             <div className="b-error_image-wrapper">
-                <img src="images/error.svg" alt="error" className="b-error_image" />
+                <img src={`${import.meta.env.VITE_PUBLIC_URL}images/error.svg`} alt="error" className="b-error_image" />
             </div>
             <div className="b-error_text">Something get wrong. <br/> Try again later!</div>
         </div>
--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -3,7 +3,7 @@ import "./loading.scss";
 const Loading = () => {
     return (
         <div className="b-loading">
-            <img src="images/loading.svg" alt="loading..." className="b-loading_image"/>
+            <img src={`${import.meta.env.VITE_PUBLIC_URL}images/loading.svg`} alt="loading..." className="b-loading_image"/>
         </div>
     )
 }
